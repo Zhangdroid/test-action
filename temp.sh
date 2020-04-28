fac296342aecfb9e0536e2fcd78c61516a3486f8
Zhuo Zhang <contrary.zhuo@gmail.com>
2020-04-28 13:19:22 +0800

    Delete temp.sh

--- temp.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST https://api.github.com/repos/Zhangdroid/test-icons/dispatches \
--H 'Accept: application/vnd.github.v3+json' \
--H 'Authorization: token 69736983eb1e82c8e3c3aec04353e2745ce9977a' \
---data '{"event_type": "CUSTOM_ACTION_NAME_HERE"}'
